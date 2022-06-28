5afe10e9c819fad967f68a958172ced28122441a
Maksym32 <95415139+Maksym32@users.noreply.github.com>
2022-06-28 09:37:42 +0000

    Deploying to gh-pages from @ Maksym32/goit-js-hw-09@d48540dd11c219bdbab68909892bfef50a31de16 🚀

---
+++ 01-color-switcher.8d71f4a4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;function o(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){t.setAttribute("disabled",!0),n=setInterval(o,1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.8d71f4a4.js.map
